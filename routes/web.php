37804fb35311a790381bccd0f3a05b1de82a16dc
HoangKhoi98 <khoihoang.261098@gmail.com>
2022-02-11 22:19:24 +0700

    11/2-2

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (){
-    return view('main_public.index');
+    return view('layouts.master');
 });
 Route::get('/contact',function (){
     return view('main_public.contact');
