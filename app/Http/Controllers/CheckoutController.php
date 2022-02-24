f04613d9dc4047ff49f0b12be01950987aa75ea6
Nam208 <82856439+Nam208@users.noreply.github.com>
2022-02-24 19:55:36 +0700

    nam

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
-use App\Cart;
 use App\Models\OrderDetail;
-use App\Models\Product;
 use Session;
 
 
